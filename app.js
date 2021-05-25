e270162189b0485037fe16bbc676aa742fdb103b
Matildad <mtld.desouza@gmail.com>
2021-05-25 13:03:06 +0200

    converting functions and communication between fields

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const scaleNames = {
     f: "Fahrenheit"
 }
 
+/**
+ * Converting functions
+ * 
+ * T(°F) = T(°C) x 9/5 + 32
+ * T(°F) = T(°C) x 1.8 + 32
+ * T(°C) = (T(°F) - 32) x 5/9
+ */
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32
+}
+
 function BoilingVerdict({celsius}) {
     if (celsius >=100) {
         return <div className='alert alert-success'>The water boils</div>
@@ -16,18 +32,18 @@ function BoilingVerdict({celsius}) {
 class TemperatureInput extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {temperature: ''}
         this.handleChange =this.handleChange.bind(this)
 
     }
 
        // method to change the value in the field
     handleChange(e) {
-        this.setState({temperature: e.target.value})
+        // this.setState({temperature: e.target.value})
+        this.props.onTemperatureChange(e.target.value)
     }
 
     render() {
-        const {temperature} = this.state
+        const {temperature} = this.props
         const name= "scale" + this.props.scale
         const scaleName = scaleNames[this.props.scale]
         return <div className="form-group">
@@ -42,16 +58,23 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            temperature: ''
+            temperature: 20
         }
+    this.handleTemperatureChange = this.handleTemperatureChange.bind(this)
+    }
+
+    handleTemperatureChange (temperature) {
+        this.setState({temperature})
     }
 
 
     render() {
         const {temperature} = this.state
+        const celsius = temperature
+        const fahrenheit = toFahrenheit(celsius)
         return <div>
-            <TemperatureInput scale="c" />
-            <TemperatureInput scale="f" />
+            <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleTemperatureChange}/>
+            <TemperatureInput scale="f" temperature={fahrenheit}/>
             <BoilingVerdict celsius={parseFloat(temperature)} />
         </div>
     }
